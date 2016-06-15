b0517341e7611faa1ae19b8f08bc981998b54aa3
Simon Butcher <simon.butcher@arm.com>
2016-06-16 00:53:38 +0100

    Adds pos-tbuild steps to Makefile to allow warnings
    A post-build step allows warnings to be displayed if the user enables the null
    entropy option.

--- library/entropy.c
+++ library/entropy.c
@@ -28,9 +28,12 @@
 #if defined(MBEDTLS_ENTROPY_C)
 
 #if defined(MBEDTLS_TEST_NULL_ENTROPY)
+
+#warning "******************************************************"
 #warning "**** WARNING!  MBEDTLS_TEST_NULL_ENTROPY defined! ****"
 #warning "**** THIS BUILD HAS NO DEFINED ENTROPY SOURCES    ****"
 #warning "**** NOT SUITABLE FOR PRODUCTION                  ****"
+#warning "******************************************************"
 #endif
 
 #include "mbedtls/entropy.h"
